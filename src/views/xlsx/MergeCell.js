ae1146c44e9a1515520e4f81fac5cc08829bfd7f
simple_lau <simple_lau@126.com>
2023-06-16 10:45:22 +0800

    feat: 🎸 将xlsx封装成类

---
+++ src/views/xlsx/MergeCell.js
@@ -0,0 +1,104 @@
+export default class MergeCell {
+  #map
+
+  #size
+
+  constructor(headers, data) {
+    this.headerMerge = []
+    this.headerValue = []
+    this.bodyMapList = []
+    this.#map = new Map()
+    this.deep = 0
+    this.init(headers, data)
+  }
+
+  /** 数据初始化 */
+  init(headers, data) {
+    this.#size = this.getCellsSize(headers)
+    this.getBodyMapList(headers)
+    this.mergeHeadersCell(headers, 0, 0)
+    this.getHeadersValue(headers, 0, 0)
+    this.bodyValue = data.map((item) =>
+      this.bodyMapList.map((key) => item[key])
+    )
+    this.deep = this.getDeep(headers)
+  }
+
+  /** 获取表头最大深度 */
+  getDeep(headers) {
+    return this.#map.get(headers)[0]
+  }
+
+  /** 根据节点获取深度广度 */
+  getCellsSize(list) {
+    if (this.#map.has(list)) {
+      return this.#map.get(list)
+    }
+    if (list?.length) {
+      let rows = -1
+      let cols = list.length - 1
+      list.forEach((item) => {
+        if (item.children) {
+          const size = this.getCellsSize(item.children)
+          rows = Math.max(size[0], rows)
+          cols += size[1]
+        }
+      })
+      this.#map.set(list, [rows + 1, cols])
+      return [rows + 1, cols]
+    }
+  }
+
+  /** 合并表头 */
+  mergeHeadersCell = (headers, row, col) => {
+    for (let i = 0, len = headers.length; i < len; i++) {
+      const cell = headers[i]
+      if (!cell.children?.length) {
+        if (row !== this.#size[0]) {
+          this.headerMerge.push({
+            s: { r: row, c: col + i },
+            e: { r: this.#size[0], c: col + i }
+          })
+        }
+      } else {
+        const size = this.#map.get(cell.children)
+        this.headerMerge.push({
+          s: { r: row, c: col + i },
+          e: { r: row, c: col + size[1] + i }
+        })
+        this.mergeHeadersCell(cell.children, row + 1, col + i)
+        col += size[1]
+      }
+    }
+  }
+
+  /** 表头赋值 */
+  getHeadersValue(headers, row, col) {
+    if (!this.headerValue[row]) {
+      this.headerValue[row] = new Array(col).fill('')
+    }
+    for (let i = 0, len = headers.length; i < len; i++) {
+      const cell = headers[i]
+      this.headerValue[row].push(cell.name)
+      if (cell.children?.length) {
+        const len = this.getCellsSize(cell.children)[1]
+        const emptyNameList = new Array(len).fill('')
+        this.headerValue[row].push(...emptyNameList)
+        this.getHeadersValue(cell.children, row + 1, col + i)
+      }
+    }
+  }
+
+  /** 正文prop对应 */
+  getBodyMapList(list) {
+    if (list?.length) {
+      list.forEach((item) => {
+        if (!item.children) {
+          this.bodyMapList.push(item.prop)
+        } else {
+          this.getBodyMapList(item.children)
+        }
+      })
+    }
+  }
+}
--- src/views/xlsx/index.vue
+++ src/views/xlsx/index.vue
@@ -4,6 +4,7 @@
 
 <script setup name="XlsxExample">
 import { utils, writeFileXLSX } from 'xlsx'
+import MergeCell from './MergeCell.js'
 
 const mergedCells = [
   {
@@ -90,97 +91,10 @@ const list = [
   }
 ]
 
-const getCellsPosition = (headers, bodys) => {
-  const map = new Map()
-
-  const getCellsSize = (list) => {
-    if (map.has(list)) {
-      return map.get(list)
-    }
-    if (list?.length) {
-      let rows = -1
-      let cols = list.length - 1
-      list.forEach((item) => {
-        if (item.children) {
-          const size = getCellsSize(item.children)
-          rows = Math.max(size[0], rows)
-          cols += size[1]
-        }
-      })
-      map.set(list, [rows + 1, cols])
-      return [rows + 1, cols]
-    }
-  }
-
-  const size = getCellsSize(headers)
-  const headerMerge = []
-  const mergeHeadersCell = (headers, row, col) => {
-    for (let i = 0, len = headers.length; i < len; i++) {
-      const cell = headers[i]
-      if (!cell.children?.length) {
-        if (row !== size[0]) {
-          headerMerge.push({
-            s: { r: row, c: col + i },
-            e: { r: size[0], c: col + i }
-          })
-        }
-      } else {
-        const size = map.get(cell.children)
-        headerMerge.push({
-          s: { r: row, c: col + i },
-          e: { r: row, c: col + size[1] + i }
-        })
-        mergeHeadersCell(cell.children, row + 1, col + i)
-        col += size[1]
-      }
-    }
-  }
-  const headerValue = []
-
-  const getHeadersValue = (headers, row, col) => {
-    if (!headerValue[row]) {
-      headerValue[row] = new Array(col).fill('')
-    }
-    for (let i = 0, len = headers.length; i < len; i++) {
-      const cell = headers[i]
-      headerValue[row].push(cell.name)
-      if (cell.children?.length) {
-        const len = getCellsSize(cell.children)[1]
-        const emptyNameList = new Array(len).fill('')
-        headerValue[row].push(...emptyNameList)
-        getHeadersValue(cell.children, row + 1, col + i)
-      }
-    }
-  }
-
-  const bodyMapList = []
-  const getBodyMapList = (list) => {
-    if (list?.length) {
-      list.forEach((item) => {
-        if (!item.children) {
-          bodyMapList.push(item.prop)
-        } else {
-          getBodyMapList(item.children)
-        }
-      })
-    }
-  }
-
-  getBodyMapList(headers)
-  mergeHeadersCell(headers, 0, 0)
-  getHeadersValue(headers, 0, 0)
-
-  return {
-    headerMerge,
-    headerValue,
-    bodyValue: bodys.map((item) => bodyMapList.map((key) => item[key])),
-    deep: map.get(headers)[0]
-  }
-}
 // 导出多表头结构
 function exportFile() {
   const ws = utils.json_to_sheet([])
-  const { headerValue, headerMerge, bodyValue, deep } = getCellsPosition(
+  const { headerValue, headerMerge, bodyValue, deep } = new MergeCell(
     mergedCells,
     list
   )
